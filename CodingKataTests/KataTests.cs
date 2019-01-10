258564a221c968a751ace07bc28d9ccd033d49bc
TheAwesomeARay <andrewray4@gmail.com>
2019-01-10 10:01:54 -0600

    CountingBits

--- CodingKataTests/KataTests.cs
+++ CodingKataTests/KataTests.cs
@@ -27,5 +27,21 @@ namespace CodingKataTests
             Assert.False(ExesAndOhsSolution.XO("Oo"));
             Assert.False(ExesAndOhsSolution.XO("ooom"));
         }
+
+        [Fact]
+        public void CountBitsTest()
+        {
+            Assert.Equal(0, BitCounting.CountBits(0));
+            Assert.Equal(1, BitCounting.CountBits(4));
+            Assert.Equal(3, BitCounting.CountBits(7));
+            Assert.Equal(2, BitCounting.CountBits(9));
+            Assert.Equal(2, BitCounting.CountBits(10));
+
+            Assert.Equal(0, BitCountingSolution.CountBits(0));
+            Assert.Equal(1, BitCountingSolution.CountBits(4));
+            Assert.Equal(3, BitCountingSolution.CountBits(7));
+            Assert.Equal(2, BitCountingSolution.CountBits(9));
+            Assert.Equal(2, BitCountingSolution.CountBits(10));
+        }
     }
 }
---
+++ CodingKatas/BitCounting.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace CodingKatas
+{
+    public static class BitCounting
+    {
+        private static byte one => Encoding.Unicode.GetBytes("1")[0];
+
+        public static int CountBits(int n)
+        {
+            var bytes = Encoding.Unicode.GetBytes(Convert.ToString(n, 2));
+            return bytes.Count(x => x == one);
+        }
+    }
+
+    public static class BitCountingSolution
+    {
+        public static int CountBits(int n)
+        {
+            return Convert.ToString(n, 2).Count(x => x == '1');
+        }
+    }
+}
