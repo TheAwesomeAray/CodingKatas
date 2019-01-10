60fe05a161291a10aa0c93aaf5184a83aa31715e
TheAwesomeARay <andrewray4@gmail.com>
2019-01-09 23:16:15 -0600

    ExesAndOhs

--- CodingKataTests/KataTests.cs
+++ CodingKataTests/KataTests.cs
@@ -6,10 +6,26 @@ namespace CodingKataTests
     public class KataTests
     {
         [Fact]
-        public void DescendingOrder()
+        public void DescendingOrderTest()
         {
             Assert.Equal(54321, DescendingOrderKata.DescendingOrder(12345));
             Assert.Equal(54321, DescendingOrderKataSolution.DescendingOrder(12345));
+        }
+
+        [Fact]
+        public void ExesAndOhsTest()
+        {
+            Assert.True(ExesAndOhs.XO("xo"));
+            Assert.True(ExesAndOhs.XO("xxOo"));
+            Assert.False(ExesAndOhs.XO("xxxm"));
+            Assert.False(ExesAndOhs.XO("Oo"));
+            Assert.False(ExesAndOhs.XO("ooom"));
+
+            Assert.True(ExesAndOhsSolution.XO("xo"));
+            Assert.True(ExesAndOhsSolution.XO("xxOo"));
+            Assert.False(ExesAndOhsSolution.XO("xxxm"));
+            Assert.False(ExesAndOhsSolution.XO("Oo"));
+            Assert.False(ExesAndOhsSolution.XO("ooom"));
         }
     }
 }
---
+++ CodingKatas/ExesAndOhs.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+
+namespace CodingKatas
+{
+    public static class ExesAndOhs
+    {
+        public static bool XO(string input)
+        {
+            var upperInput = input.ToUpper();
+            return upperInput.Split('O').Count() == upperInput.Split('X').Count();
+        }
+    }
+
+    public static class ExesAndOhsSolution
+    {
+        public static bool XO(string input)
+        {
+            return input.ToLower().Count(x => x == 'x') == input.ToLower().Count(x => x == 'o');
+        }
+    }
+}
