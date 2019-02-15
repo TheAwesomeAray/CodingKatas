9d3133a4490ec8cce0638d4322f967651545fc73
TheAwesomeARay <andrewray4@gmail.com>
2019-02-15 17:34:58 -0600

    FileLogger calls writeToFile

--- CodingKataTests/FileLoggerTests.cs
+++ CodingKataTests/FileLoggerTests.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CodingKatas;
+using Moq;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using Xunit;
@@ -8,9 +10,15 @@ namespace CodingKataTests
     public class FileLoggerTests
     {
         [Fact]
-        public void Test1()
+        public void Log_Message_WritesMessageToFile()
         {
+            string message = "test message";
+            var writer = new Mock<IFileWriter>();
+            var fileLogger = new FileLogger(writer.Object);
 
+            fileLogger.Log(message);
+
+            writer.Verify(x => x.WriteToFile(It.Is<string>(y => y == message)));
         }
     }
 }
--- CodingKatas/FileLogger.cs
+++ CodingKatas/FileLogger.cs
@@ -2,9 +2,24 @@
 {
     public class FileLogger
     {
+        private IFileWriter writer;
+
+        public FileLogger(IFileWriter writer)
+        {
+            this.writer = writer;
+        }
+
         public void Log(string message)
         {
-
+            using (System.IO.StreamWriter file = new System.IO.StreamWriter("log.txt", true))
+            {
+                writer.WriteToFile(message);
+            }
         }
+    }
+
+    public interface IFileWriter
+    {
+        void WriteToFile(string message);
     }
 }
