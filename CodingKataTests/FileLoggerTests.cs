4875e8f90076999e97d6b689d902255c28dc6de2
TheAwesomeARay <andrewray4@gmail.com>
2019-02-16 16:20:06 -0600

    Added additional test cases

--- CodingKataTests/FileLoggerTests.cs
+++ CodingKataTests/FileLoggerTests.cs
@@ -22,17 +22,20 @@ namespace CodingKataTests
             writer.Verify(x => x.WriteToFile(It.Is<string>(y => y == message), It.IsAny<string>()));
         }
 
-        [Fact]
-        public void Log_Message_AppendsDateToFileName()
+        [Theory]
+        [InlineData(1970, 12, 31, "log19701231.txt")]
+        [InlineData(1999, 05, 25, "log19990525.txt")]
+        [InlineData(2025, 1, 01, "log20250101.txt")]
+        public void Log_Message_AppendsDateToFileName(int year, int month, int day, string expected)
         {
             var writer = new Mock<IFileWriter>();
             var date = new Mock<ISystemDate>();
-            date.Setup(x => x.Now()).Returns(new DateTime(1999, 05, 25));
+            date.Setup(x => x.Now()).Returns(new DateTime(year, month, day));
             var fileLogger = new FileLogger(writer.Object, date.Object);
 
             fileLogger.Log("");
 
-            writer.Verify(x => x.WriteToFile(It.IsAny<string>(), It.Is<string>(y => y == "log19990525.txt")));
+            writer.Verify(x => x.WriteToFile(It.IsAny<string>(), It.Is<string>(y => y == expected)));
         }
     }
 }
