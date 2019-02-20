9e523d3421a176b399ff96364454fe74f74314ee
TheAwesomeARay <andrewray4@gmail.com>
2019-02-19 23:36:59 -0600

    Reverse Integer

---
+++ CodingKataTests/ReverseIntegerTests.cs
@@ -0,0 +1,27 @@
+﻿using CodingKatas;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+public class ReverseIntegerTests
+{
+    [Theory]
+    [InlineData(1, 1)]
+    [InlineData(12, 21)]
+    [InlineData(120, 21)]
+    public void Reverse_GivenPositiveInteger_ReturnsReversedInteger(int input, int expected)
+    {
+        new ReverseIntegerKata().Reverse(input).Should().Be(expected);
+    }
+
+    [Theory]
+    [InlineData(-1, -1)]
+    [InlineData(-12, -21)]
+    public void Reverse_GivenNegativeInteger_ReturnsReversedInteger(int input, int expected)
+    {
+        new ReverseIntegerKata().Reverse(input).Should().Be(expected);
+    }
+}
+
---
+++ CodingKatas/ReverseInteger.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace CodingKatas
+{
+    public class ReverseIntegerKata
+    {
+        public int Reverse(int x)
+        {
+            bool negative = x < 0;
+
+            var arr = Math.Abs(x).ToString()
+                       .ToCharArray()
+                       .Reverse();
+
+            var reversedInteger = int.Parse(new string(arr.ToArray()));
+
+            if (negative)
+                return reversedInteger * -1;
+            
+
+            return reversedInteger;
+        }
+    }
+}
