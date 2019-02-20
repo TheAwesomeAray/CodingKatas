81cbc42d078086d157ac30b7b9002490407cc8d2
TheAwesomeARay <andrewray4@gmail.com>
2019-02-19 23:07:10 -0600

    TwoSum

---
+++ CodingKataTests/TwoSumTests.cs
@@ -0,0 +1,19 @@
+﻿using CodingKatas;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using FluentAssertions;
+
+public class TwoSumTests
+{
+    [Theory]
+    [InlineData(new[] { 1, 0 }, 1, new[] { 0, 1 })]
+    [InlineData(new[] { 1, 2, 3 }, 4, new[] { 0, 2 })]
+    [InlineData(new[] { 1, 15, 25, 100 }, 125, new[] { 2, 3 })]
+    public void TwoSum_GivenArrayAndTarget_ReturnsIndicesThatEqualTarget(int[] input, int target, int[] expected)
+    {
+        expected.Should().BeEquivalentTo(new TwoSumKata().TwoSum(input, target));
+    }
+}
+
---
+++ CodingKatas/TwoSum.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CodingKatas
+{
+    public class TwoSumKata
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            return OnePassHashTable(nums, target);
+        }
+
+        private int[] BruteForce(int[] nums, int target)
+        {
+            for (int i = 0; i < nums.Length; i++)
+                for (int j = i + 1; j < nums.Length; j++)
+                    if (nums[j] == target - nums[i])
+                        return new[] { i, j };
+
+            throw new InvalidOperationException("No Solution Exists");
+        }
+
+        private int[] TwoPassHashTable(int[] nums, int target)
+        {
+            var numberDict = new Dictionary<int, int>();
+            //Convert int array to hashtable?
+            for (int i = 0; i < nums.Length && !numberDict.ContainsKey(nums[i]); i++)
+                numberDict.Add(nums[i], i);
+
+            for (int i = 0; i < nums.Length; i++)
+                if (numberDict.ContainsKey(target - nums[i]))
+                    return new int[] { i, numberDict[target - nums[i]]};
+
+            throw new InvalidOperationException("No Solution Exists");
+        }
+
+        private int[] OnePassHashTable(int[] nums, int target)
+        {
+            var numberDict = new Dictionary<int, int>();
+            //Convert int array to hashtable?
+            for (int i = 0; i < nums.Length && !numberDict.ContainsKey(nums[i]); i++)
+            {
+                int complement = target - nums[i];
+                if (numberDict.ContainsKey(complement))
+                    return new[] { i, numberDict[complement] };
+
+                numberDict.Add(nums[i], i);
+            }
+
+            throw new InvalidOperationException("No Solution Exists");
+        }
+    }
+}
