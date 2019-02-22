d97a6ee868c1d84cdf37a65de9e4c6bf4abd98bd
TheAwesomeARay <andrewray4@gmail.com>
2019-02-21 22:52:25 -0600

    Removed duplication of hexidecimal expression

--- CodingKatas/HTOI.cs
+++ CodingKatas/HTOI.cs
@@ -8,8 +8,8 @@ namespace CodingKatas
     {
         public double Htoi(string input)
         {
-            int i, n;
-            i = n = 0;
+            int i, n, hexValue;
+            i = n = hexValue = 0;
 
             if (input[i] == '0')
             {
@@ -21,11 +21,15 @@ namespace CodingKatas
             for (; i < input.Length; i++)
             {
                 if (input[i] >= '0' && input[i] <= '9')
-                    n = n * 16 + input[i] - '0';
+                    hexValue = input[i] - '0';
                 else if (input[i] >= 'A' && input[i] <= 'F')
-                    n = n * 16 + input[i] - 'A' + 10;
+                    hexValue = input[i] - 'A' + 10;
+
+                n = n * 16 + hexValue;
             }
-            
+
+           
+
 
             return n;
         }
