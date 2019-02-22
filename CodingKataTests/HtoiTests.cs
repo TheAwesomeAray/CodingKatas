a55c9fa45421c10ce23222937fcc931832e86c50
TheAwesomeARay <andrewray4@gmail.com>
2019-02-21 22:36:21 -0600

    initial test

---
+++ CodingKataTests/HtoiTests.cs
@@ -0,0 +1,15 @@
+﻿using CodingKatas;
+using FluentAssertions;
+using Xunit;
+
+public class HtoiTests
+{
+    [Theory]
+    [InlineData("0x0", 0)]
+    [InlineData("0x1", 1)]
+    public void Htoi_GivenHexidecimalValue_ReturnsConvertedInteger(string input, int expected)
+    {
+        new HtoiKata().Htoi(input).Should().Be(expected);
+    }
+}
+
---
+++ CodingKatas/HTOI.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodingKatas
+{
+    public class HtoiKata
+    {
+        public double Htoi(string input)
+        {
+            int i, n;
+            i = n = 0;
+
+            if (input[i] == '0')
+            {
+                i++;
+                if (input[i] == 'x')
+                    i++;
+            }
+
+            if (input[i] >= '0' && input[i] <= '9')
+                n = input[i] - '0';
+
+            return n;
+        }
+    }
+}
