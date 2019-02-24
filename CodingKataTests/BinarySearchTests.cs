96b2f99d389cbd1db828de0b050d0188946d152d
TheAwesomeARay <andrewray4@gmail.com>
2019-02-24 16:35:16 -0600

    Binary Search

---
+++ CodingKataTests/BinarySearchTests.cs
@@ -0,0 +1,25 @@
+﻿using CodingKatas;
+using FluentAssertions;
+using Xunit;
+
+public class BinarySearchTests
+{
+    [Theory]
+    [InlineData(1, new[] { 2 })]
+    [InlineData(4, new[] { 2, 3 })]
+    [InlineData(5, new[] { 1, 2, 3, 4, 6, 7, 8, 9 })]
+    public void Search_ValueNotInArray_ReturnsNegative1(int searchTerm, int[] searchArray)
+    {
+        new BinarySearchKata().Search(searchTerm, searchArray, searchArray.Length).Should().Be(-1);
+    }
+
+    [Theory]
+    [InlineData(2, new[] { 2 }, 0)]
+    [InlineData(4, new[] { 2, 3, 4 }, 2)]
+    [InlineData(5, new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 }, 4)]
+    public void Search_ValueInArray_ReturnsIndexOfValue(int searchTerm, int[] searchArray, int expected)
+    {
+        new BinarySearchKata().Search(searchTerm, searchArray, searchArray.Length).Should().Be(expected);
+    }
+}
+
---
+++ CodingKatas/BinarySearch.cs
@@ -0,0 +1,27 @@
+﻿namespace CodingKatas
+{
+    public class BinarySearchKata
+    {
+        public int Search(int searchTerm, int[] v, int length)
+        {
+            int low, high, mid;
+
+            low = 0;
+            high = length - 1;
+            mid = (low + high) / 2;
+
+            while (low <= high && searchTerm != v[mid])
+            {
+                if (searchTerm < v[mid])
+                    high = mid - 1;
+                else
+                    low = mid + 1;
+                mid = (low + high) / 2;
+            }
+            if (searchTerm == v[mid])
+                return mid;
+            else
+                return -1;
+        }
+    }
+}
