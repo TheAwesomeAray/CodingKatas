ee0152f41ec6ae266ed8027f462e11d33b5c8456
TheAwesomeARay <andrewray4@gmail.com>
2019-02-24 16:44:17 -0600

    Added Binary Search, reorganized tests

---
+++ CodingKatas/CProgrammingLanguage/Escape.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodingKatas
+{
+    public class EscapeKata
+    {
+        public string Escape(string s)
+        {
+            return s;
+        }
+    }
+}
