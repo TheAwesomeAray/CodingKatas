bcb2ce0cfd9dc3c140bb9c49f95413e378d8668a
TheAwesomeARay <andrewray4@gmail.com>
2019-02-25 16:31:54 -0600

    Plus operator

--- CodingKatas/CProgrammingLanguage/ReversePolishCalculator.cs
+++ CodingKatas/CProgrammingLanguage/ReversePolishCalculator.cs
@@ -6,7 +6,7 @@ namespace CodingKatas.CProgrammingLanguage
 {
     public class ReversePolishCalculator
     {
-        double[] val;
+        string[] val;
         int sp = 0;
 
         public double Calculate(string input)
@@ -16,18 +16,28 @@ namespace CodingKatas.CProgrammingLanguage
 
             for (int i = 0; i < input.Length; i++)
             {
-                double op1 = Pop();
-                double op2 = Pop();
+                double op1 = ConvertToDigit(Pop());
+                double op2 = ConvertToDigit(Pop());
+                string op = Pop();
+                switch (op)
+                {
+                    case "+":
+                        return op1 + op2;
+                    default:
+                        throw new InvalidOperationException("Invalid Operator");
+
+                }
+
                 return op1 + op2;
             }
 
             return 0;
         }
 
-        private double[] Split(string input)
+        private string[] Split(string input)
         {
             int spaceCount = GetSpaceCount(input);
-            double[] result = new double[spaceCount + 1];
+            string[] result = new string[spaceCount + 1];
             int j = input.Length - 1;
             for(int i = 0; i < result.Length; i++)
             {
@@ -38,7 +48,7 @@ namespace CodingKatas.CProgrammingLanguage
                 }
                     j--;
 
-                result[i] = ConvertToDigit(expressionComponent);
+                result[i] = expressionComponent;
             }
 
             return result;
@@ -65,13 +75,13 @@ namespace CodingKatas.CProgrammingLanguage
             return result;
         }
 
-        private void Push(double value)
+        private void Push(string value)
         {
             if (sp < val.Length - 1)
                 val[sp++] = value;
         }
 
-        private double Pop()
+        private string Pop()
         {
             if (sp > 0)
                 return val[--sp];
