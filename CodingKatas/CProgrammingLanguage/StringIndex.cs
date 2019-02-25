10239a41ea04eaa81ee346e9a51b39d06450471d
TheAwesomeARay <andrewray4@gmail.com>
2019-02-24 19:23:01 -0600

    StringIndex

---
+++ CodingKatas/CProgrammingLanguage/StringIndex.cs
@@ -0,0 +1,20 @@
+﻿namespace CodingKatas.CProgrammingLanguage
+{
+    public class StringIndexKata
+    {
+        public int StringIndex(string s, string t)
+        {
+            for (int i = s.Length - 1; i >= 0; i--)
+            {
+                int j, k;
+                for (j = t.Length - 1, k = i; j >= 0 && s[k] == t[j]; j--, k--)
+                    ;
+
+                if (j == -1)
+                    return k + 1;
+            }
+
+            return -1;
+        }
+    }
+}
---
+++ CodingKatas/CProgrammingLanguage/StringIndexTests.cs
@@ -0,0 +1,35 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace CodingKatas.CProgrammingLanguage
+{
+    public class StringIndexTests
+    {
+        [Theory]
+        [InlineData("12345", "3", 2)]
+        [InlineData("12345", "34", 2)]
+        [InlineData("The quick brown fox", "brown", 10)]
+        public void StringIndex_ReturnsIndexOfString(string input, string searchTerm, int expected)
+        {
+            new StringIndexKata().StringIndex(input, searchTerm).Should().Be(expected);
+        }
+
+        [Theory]
+        [InlineData("32345", "3", 2)]
+        [InlineData("342345", "34", 3)]
+        [InlineData("test quick test fox", "test", 11)]
+        public void StringIndex_ReturnsRightmostIndexOfString(string input, string searchTerm, int expected)
+        {
+            new StringIndexKata().StringIndex(input, searchTerm).Should().Be(expected);
+        }
+
+        [Theory]
+        [InlineData("32345", "6")]
+        [InlineData("342345", "9")]
+        [InlineData("test quick test fox", "test2")]
+        public void StringIndex_ReturnsNegativeOneIfNoMatchFound(string input, string searchTerm)
+        {
+            new StringIndexKata().StringIndex(input, searchTerm).Should().Be(-1);
+        }
+    }
+}
