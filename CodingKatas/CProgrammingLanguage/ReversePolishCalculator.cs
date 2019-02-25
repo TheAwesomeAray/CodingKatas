88280ff5a41db08df065f5329545bf79188b5dc3
TheAwesomeARay <andrewray4@gmail.com>
2019-02-24 20:06:46 -0600

    My red green is in need of a refactor

---
+++ CodingKatas/CProgrammingLanguage/ReversePolishCalculator.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodingKatas.CProgrammingLanguage
+{
+    public class ReversePolishCalculator
+    {
+        double[] val;
+        int sp = 0;
+
+        public double Calculate(string input)
+        {
+            val = Split(input);
+            sp = val.Length;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                double op1 = Pop();
+                double op2 = Pop();
+                return op1 + op2;
+            }
+
+            return 0;
+        }
+
+        private double[] Split(string input)
+        {
+            int spaceCount = GetSpaceCount(input);
+            double[] result = new double[spaceCount + 1];
+            int j = input.Length - 1;
+            for(int i = 0; i < result.Length; i++)
+            {
+                string expressionComponent = "";
+                for (; j >= 0 && input[j] != ' '; j--)
+                {
+                    expressionComponent += input[j];
+                }
+                    j--;
+
+                result[i] = ConvertToDigit(expressionComponent);
+            }
+
+            return result;
+        }
+
+        public int GetSpaceCount(string input)
+        {
+            int k = 0;
+            for (int i = 0; i < input.Length; i++)
+                if (input[i] == ' ')
+                    k++;
+
+            return k;
+        }
+
+        private double ConvertToDigit(string input)
+        {
+            double result = 0;
+            for (int i = input.Length - 1; i >= 0; i--)
+            {
+                result = result * 10 + input[i] - '0';
+            }
+
+            return result;
+        }
+
+        private void Push(double value)
+        {
+            if (sp < val.Length - 1)
+                val[sp++] = value;
+        }
+
+        private double Pop()
+        {
+            if (sp > 0)
+                return val[--sp];
+            else
+                throw new InvalidOperationException("Stack Empty");
+
+            
+        }
+    }
+}
---
+++ CodingKatas/CProgrammingLanguage/ReversePolishCalculatorTests.cs
@@ -0,0 +1,29 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace CodingKatas.CProgrammingLanguage
+{
+    public class ReversePolishCalculatorTests
+    {
+        [Theory]
+        [InlineData("1 2 +", 3)]
+        [InlineData("5 4 +", 9)]
+        [InlineData("9 9 +", 18)]
+        public void Calculate_GivenSingleDigitOperandsAndPlusSign_ReturnsSum(string input, double expected)
+        {
+            new ReversePolishCalculator().Calculate(input).Should().Be(expected);
+        }
+
+        [Theory]
+        [InlineData("11 2 +", 13)]
+        [InlineData("5 45 +", 50)]
+        [InlineData("300 350 +", 650)]
+        public void Calculate_GivenMultipleDigitOperandsAndPlusSign_ReturnsSum(string input, double expected)
+        {
+            new ReversePolishCalculator().Calculate(input).Should().Be(expected);
+        }
+    }
+}
