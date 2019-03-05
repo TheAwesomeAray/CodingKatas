798935834a3c57ef15c834d5b4512460bb633199
TheAwesomeARay <andrewray4@gmail.com>
2019-03-04 22:31:19 -0600

    Atoi - initial case

---
+++ CodingKatas/LeetCoders/AtoiKata.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodingKatas.LeetCoders
+{
+    public class AtoiKata
+    {
+        public int Atoi(string input)
+        {
+            var character = input[0];
+
+            return character - '0';
+        }
+    }
+}
---
+++ CodingKatas/LeetCoders/AtoiTests.cs
@@ -0,0 +1,14 @@
+﻿using CodingKatas.LeetCoders;
+using FluentAssertions;
+using Xunit;
+
+public class AtoiTests
+{
+    [Theory]
+    [InlineData("1", 1)]
+    public void Atoi_ConvertsStringToInteger(string input, int expected)
+    {
+        new AtoiKata().Atoi(input).Should().Be(expected);
+    }
+}
+
