4b0cb8d180e96882956f5c9c96e090fdef58f362
TheAwesomeARay <andrewray4@gmail.com>
2019-03-08 22:49:46 -0600

    Final rename

--- CodingKatas/LeetCoders/MergeSortedListTests.cs
+++ CodingKatas/LeetCoders/MergeSortedListTests.cs
@@ -9,9 +9,11 @@ namespace CodingKatas.LeetCoders
         [Theory]
         [InlineData(new[] { 1, 2, 3}, new[] { 4, 5, 6 },
             new[] { 1, 2, 3, 4, 5, 6 })]
-        public void Sort_ReturnsSortedList(int[] a, int[] b, int[] expected)
+        public void Sort_ReturnsMergedList(int[] a, int[] b, int[] expected)
         {
             new MergeSortedListKata().Merge(a.ToList(), b.ToList()).Should().BeEquivalentTo(expected.ToList());
         }
+
+        public void 
     }
 }
