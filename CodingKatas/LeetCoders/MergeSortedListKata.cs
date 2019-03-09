d0906203950ecdb8121da6fe9123dc84ab42ec93
TheAwesomeARay <andrewray4@gmail.com>
2019-03-08 22:37:34 -0600

    Initial pass

--- CodingKatas/LeetCoders/MergeSortedListKata.cs
+++ CodingKatas/LeetCoders/MergeSortedListKata.cs
@@ -8,7 +8,35 @@ namespace CodingKatas.LeetCoders
     {
         public List<int> Sort(List<int> a, List<int> b)
         {
-            return new List<int>();
+            var mergedList = new List<int>();
+            int i = 0, j = 0;
+            while (i < a.Count || j < b.Count)
+            {
+                if (i == a.Count)
+                {
+                    while (j < b.Count)
+                        mergedList.Add(b[j++]);
+
+                    return mergedList;
+                }
+                    
+                if (j == b.Count)
+                {
+                    while (j < b.Count)
+                        mergedList.Add(b[j++]);
+
+                    return mergedList;
+                }
+                   
+                if (a[i] < b[j])
+                    mergedList.Add(a[i++]);
+                else
+                    mergedList.Add(b[j++]);
+            
+            }
+                
+            
+            return mergedList;
         }
     }
 }
