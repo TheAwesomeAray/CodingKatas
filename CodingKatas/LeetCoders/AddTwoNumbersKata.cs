e34dde16f73b223bf3bb9890811799557c1ec758
TheAwesomeARay <andrewray4@gmail.com>
2019-03-10 21:38:43 -0500

    Just getting started

--- CodingKatas/LeetCoders/AddTwoNumbersKata.cs
+++ CodingKatas/LeetCoders/AddTwoNumbersKata.cs
@@ -8,9 +8,23 @@ namespace CodingKatas.LeetCoders
     {
         public LinkedList<int> AddTwoNumbers(LinkedList<int> a, LinkedList<int> b)
         {
+            var aEnum = a.GetEnumerator();
+            var bEnum = b.GetEnumerator();
+            LinkedList<int> result = new LinkedList<int>();
 
+            while (aEnum.MoveNext())
+            //bool carry = false;
+            int intermediateSum = aEnum.Current + bEnum.Current;
 
-            return new LinkedList<int>();
+            if (intermediateSum > 9)
+            {
+                result.AddFirst(intermediateSum % 10);
+                //carry = true;
+            }
+            else
+                result.AddFirst(intermediateSum);
+
+            return result;
         }
     }
 }
