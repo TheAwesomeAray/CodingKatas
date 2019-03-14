68f7d92a8cbf9141d2f1d41bb517ad9bea48fb39
TheAwesomeARay <andrewray4@gmail.com>
2019-03-13 19:11:27 -0500

    Iteration 1

---
+++ CodingKatas/SteveSmith/RPGCombat.cs
@@ -0,0 +1,29 @@
+﻿namespace CodingKatas.SteveSmith
+{
+    public class RPGCombat
+    {
+        public class Character
+        {
+            public bool Alive { get; private set; } = true;
+            public int Health { get; private set; } = 1000;
+            public int Level { get; private set; } = 1;
+
+            public void Attack(Character target)
+            {
+                target.Health -= 200;
+
+                if (target.Health <= 0)
+                    target.Alive = false;
+            }
+
+            public void Heal(Character target)
+            {
+                if (target.Health + 200 > 1000)
+                    target.Health = 1000;
+                else
+                    target.Health += 200;
+            }
+        }
+
+    }
+}
---
+++ CodingKatas/SteveSmith/RPGCombatTests.cs
@@ -0,0 +1,68 @@
+﻿using FluentAssertions;
+using Xunit;
+using static CodingKatas.SteveSmith.RPGCombat;
+
+namespace CodingKatas.SteveSmith
+{
+    public class RPGCombatTests
+    {
+        [Fact]
+        public void Character_CreatedWithAppropriateDefaultValues()
+        {
+            var character = new Character();
+
+            character.Health.Should().Be(1000);
+            character.Alive.Should().Be(true);
+            character.Level.Should().Be(1);
+        }
+
+        [Fact]
+        public void Attack_DamagesTargetCharacter()
+        {
+            var attackingCharacter = new Character();
+            var targetCharacter = new Character();
+
+            attackingCharacter.Attack(targetCharacter);
+
+            targetCharacter.Health.Should().Be(800);
+        }
+
+        [Fact]
+        public void Heal_IncreasesHealthOfTargetCharacter()
+        {
+            var healingCharacter = new Character();
+            var targetCharacter = new Character();
+            healingCharacter.Attack(targetCharacter);
+            
+            healingCharacter.Heal(targetCharacter);
+
+            targetCharacter.Health.Should().Be(1000);
+        }
+
+        [Fact]
+        public void Heal_CannotRaiseHealthAbove1000()
+        {
+            var healingCharacter = new Character();
+            var targetCharacter = new Character();
+
+            healingCharacter.Heal(targetCharacter);
+
+            targetCharacter.Health.Should().Be(1000);
+        }
+
+        [Fact]
+        public void Character_WhenReducedAtOrBelow0Health_AliveIsFalse()
+        {
+            var attackingCharacter = new Character();
+            var targetCharacter = new Character();
+
+            attackingCharacter.Attack(targetCharacter);
+            attackingCharacter.Attack(targetCharacter);
+            attackingCharacter.Attack(targetCharacter);
+            attackingCharacter.Attack(targetCharacter);
+            attackingCharacter.Attack(targetCharacter);
+
+            targetCharacter.Alive.Should().Be(false);
+        }
+    }
+}
