7a28e67a31900e0c7a6493b42222aa6f32daedad
TheAwesomeARay <andrewray4@gmail.com>
2019-03-19 20:34:10 -0500

    Zombie Survivors

---
+++ CodingKatas/SteveSmith/ZombieSurvivors.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodingKatas.SteveSmith
+{
+    public class Surivor
+    {
+        public int Wounds { get; set; }
+
+        public bool Alive { get; set; }
+    }
+}
